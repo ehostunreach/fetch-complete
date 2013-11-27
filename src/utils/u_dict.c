0c8681c0518d1dceb1dd017f975607f779dd8a4f
Vasilis Kalintiris <ehostunreach@gmail.com>
2013-11-27 17:13:40 +0200

    Be more strict with u_dict

--- src/utils/u_dict.c
+++ src/utils/u_dict.c
@@ -1,4 +1,5 @@
 #include <glib.h>
+#include "u_assert.h"
 #include "u_log.h"
 #include "u_alloc.h"
 #include "u_dict.h"
@@ -14,17 +15,9 @@ u_dict_init(void)
     struct u_dict *dict;
 
     ghtable = g_hash_table_new(g_str_hash, g_str_equal);
-    if (!ghtable) {
-        u_warn("Failed to create new dictionary!\n");
-        return NULL;
-    }
+    u_assert(ghtable);
 
     dict = u_malloc(sizeof(struct u_dict));
-    if (!dict) {
-        g_hash_table_destroy(ghtable);
-        return NULL;
-    }
-
     dict->ht = ghtable;
     return dict;
 }
@@ -32,6 +25,8 @@ u_dict_init(void)
 void
 u_dict_fini(struct u_dict *dict)
 {
+    u_assert(dict);
+
     g_hash_table_destroy(dict->ht);
     u_free(dict);
 }
@@ -39,12 +34,16 @@ u_dict_fini(struct u_dict *dict)
 void
 u_dict_add(struct u_dict *dict, char *key, void *data)
 {
+    u_assert(dict && key);
+
     g_hash_table_insert(dict->ht, (gpointer) key, data);
 }
 
 void *
 u_dict_get(struct u_dict *dict, const char *key)
 {
+    u_assert(dict && key);
+
     return g_hash_table_lookup(dict->ht, key);
 }
 
@@ -53,6 +52,8 @@ u_dict_iter(struct u_dict *dict, u_dict_foreach func)
 {
     GHashTableIter iter;
     gpointer key, value;
+
+    u_assert(dict && func);
 
     g_hash_table_iter_init(&iter, dict->ht);
     while (g_hash_table_iter_next(&iter, &key, &value)) {
